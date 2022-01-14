5660d4b7267270193bfcec1dd207356c597c2a24
augustine eze <augustineeze2019@gmail.com>
2022-01-14 15:36:33 +0100

    build ip address tracker UI

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,83 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container">
+  <div style="padding-bottom: 120px;" class="row pt-5">
+    <div class= "col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3 col-sm-12 position-relative">
+      <label for="validationTooltipUsername" class="form-label" style="font-size: 25px; color: white;">IP Address Tracker</label>
+      <div class="input-group">
+        <input type="text" v-model="ip" class="form-control" id="validationTooltipUsername" aria-describedby="validationTooltipUsernamePrepend">
+        <span style="background-color: #000000; border: black" class="input-group-text" id="validationTooltipUsernamePrepend"><i class="fa fa-chevron-right" aria-hidden="true"></i></span>
+      </div>
+    </div>
+  </div>
+
+    <div  class="row pt-5 ipDetails">
+      <div class= "col-xl-6 offset-xl-3 col-lg-6 offset-lg-3 col-md-6 offset-md-3 col-sm-12 position-relative">
+        <div style="background-color: white">
+          <div class="row pt-5 pb-5">
+            <div class="col-3">
+              <span class="title">IP ADDRESS</span><br/>
+              <h4>{{ip}}</h4>
+            </div>
+
+            <div class="col-3">
+              <span class="title">LOCATION</span><br/>
+              <h4>California</h4>
+            </div>
+
+            <div class="col-3">
+              <span class="title">TIMEZONE</span><br/>
+              <h4>UTC-08:00</h4>
+            </div>
+
+            <div class="col-3">
+              <span class="title">ISP</span><br/>
+              <h4>Google LLc</h4>
+            </div>
+
+          </div>
+
+        </div>
+      </div>
+      </div>
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
   name: 'Home',
-  components: {
-    HelloWorld
-  }
+  data(){
+    return{
+      ip: '8.8.8.8'
+    }
+  },
 }
 </script>
+
+<style scoped>
+.fa{
+  color: #ffffff;
+}
+
+.input-group-text:hover{
+  cursor: pointer;
+}
+
+h4{
+  font-weight: bold;
+}
+
+.title{
+  font-size: 10px;
+  font-weight: bold;
+}
+.ipDetails{
+  position: fixed;
+  z-index: 300000;
+  width: 100%;
+  left: 0% !important;
+  top: 150px;
+  margin-right: -100px!important;
+}
+</style>
